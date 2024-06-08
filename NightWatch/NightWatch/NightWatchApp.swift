de49cc8524462937637ff04bacf9b652c471f2e5
Akmal Ahmed <akmal.ahmed@itelasoft.com>
2024-06-08 12:14:36 +0530

    Created the NightWatch app project

---
+++ NightWatch/NightWatch/NightWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  NightWatchApp.swift
+//  NightWatch
+//
+//  Created by Akmal on 2024-06-08.
+//
+
+import SwiftUI
+
+@main
+struct NightWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
